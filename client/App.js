85e553fbbd0cb4b217c235e3f4a832c779bd428b
David <scriptura7773@gmail.com>
2022-05-06 18:53:17 -0700

    first commit yeti crabs

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <button className='btn btn-primary'>Test</button>
+    <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App;
